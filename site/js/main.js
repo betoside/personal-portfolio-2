83883722047a2306d5f80a9e5a72ce18b7389bf8
Betoside <betoside@gmail.com>
2022-12-13 20:11:56 -0300

    our clientes

--- site/js/main.js
+++ site/js/main.js
@@ -51,6 +51,19 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
 });
 
 //Our clients - Swiper
+var swiper = new Swiper(".client-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 //Website dark/light theme
 
