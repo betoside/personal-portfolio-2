03ec8947fb19572ada5a18973994984c6c8d6aac
Betoside <betoside@gmail.com>
2022-12-13 18:44:28 -0300

    portfolio

--- site/js/main.js
+++ site/js/main.js
@@ -27,6 +27,29 @@ modalCloseBtns.forEach((modalCloseBtn) => {
     });
 });
 
+
+const portfolioModals = document.querySelectorAll('.portfolio-model');
+const imgCards = document.querySelectorAll('.img-card');
+const portfolioCloseBtns = document.querySelectorAll('.portfolio-close-btn');
+
+let portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add('active');
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener('click', () => {
+        portfolioModal(i);
+    })
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener('click', () => {
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove('active');
+        });
+    });
+});
+
 //Our clients - Swiper
 
 //Website dark/light theme
