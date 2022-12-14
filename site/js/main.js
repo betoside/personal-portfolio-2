5258bfa3ab0045a0fd2731468a2fea206c375124
Betoside <betoside@gmail.com>
2022-12-14 13:28:21 -0300

    Website dark/light theme

--- site/js/main.js
+++ site/js/main.js
@@ -66,6 +66,33 @@ var swiper = new Swiper(".client-swiper", {
   });
 
 //Website dark/light theme
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    themeBtn.classList.toggle('sun');
+
+    localStorage.setItem('saved-theme', getCurrentTheme());
+    localStorage.setItem('saved-icon', getCurrentIcon());
+
+    // console.log('clicou: themeBtn');
+});
+
+const getCurrentTheme = () => document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+const getCurrentIcon = () => themeBtn.classList.contains('sun') ? 'sun' : 'moon';
+
+
+// console.log('savedTheme: '+localStorage.getItem('saved-theme'));
+// console.log('savedIcon: '+localStorage.getItem('saved-icon'));
+const savedTheme = localStorage.getItem('saved-theme');
+const savedIcon = localStorage.getItem('saved-icon');
+
+if(savedTheme){
+    document.body.classList[savedTheme === 'dark' ? 'add' : 'remove']('dark-theme');
+    themeBtn.classList[savedIcon === 'sun' ? 'add' : 'remove']('sun');
+
+}
+// console.log('theem: '+savedTheme, 'icon: '+savedIcon);
 
 //Scroll to top button
 const scrollToTopBtn = document.querySelector('.scrollToTop-btn');
@@ -80,6 +107,26 @@ scrollToTopBtn.addEventListener('click', () => {
 });
 
 //Navigation menu items active on page scroll
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute('id');
+
+        // console.log(id, ' - h: '+sectionHeight, ' - top: '+sectionTop);
+
+        if( scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ){
+            document.querySelector('.nav-items a[href*=' + id + ']').classList.add('active');
+        } else {
+            document.querySelector('.nav-items a[href*=' + id + ']').classList.remove('active');
+        }
+
+        // console.log('scrollY: '+scrollY, 'sectionHeight: '+sectionHeight, 'sectionTop: '+sectionTop, id, 'soma: '+sectionHeight+sectionTop );
+    });
+});
 
 //Responsive navigation menu toggle
 
